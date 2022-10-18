5986b33888dd8956dea8309bf55ba1f12a8425b3
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-10-18 18:54:00 +1000

    Add Address.py

---
+++ data/address.py
@@ -0,0 +1,10 @@
+from .covalentQuery import Chain
+
+ADDRESSES = {
+            'POOL':
+                {Chain.Optimism:'0x395Ae52bB17aef68C2888d941736A71dC6d4e125'},
+            'USDC':
+                {Chain.Optimism:'0x7F5c764cBc14f9669B88837ca1490cCa17c31607'},
+            'PTUSDC':
+                {Chain.Optimism:'0x62BB4fc73094c83B5e952C2180B23fA7054954c4'}
+            }
